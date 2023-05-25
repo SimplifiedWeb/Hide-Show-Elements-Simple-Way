cbdef2d8afffedb31174a9fe1d9304b51ce4aa1e
shaikhfaizan <faizanshaikh2002320@gmail.com>
2023-05-25 13:01:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function hidebox(){
+    var z = document.getElementById("container")
+    if(z.style.display === "none"){
+        z.style.display = 'block';
+    }else{
+        z.style.display = "none";
+    }
+}
